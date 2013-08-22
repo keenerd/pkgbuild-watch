ef5a1918e514881912be021c4e519e6d35eaa17b
Josh McGee <josh.s.mcgee@gmail.com>
2013-08-22 10:16:01 -0700

    rapid cleanup; tiny optimizations. promise it is not too complicated in the diffs =]

--- urlwatch.sh
+++ urlwatch.sh
@@ -2,15 +2,15 @@
 
 # the original urlwatch is slow and buggy
 
-if [ $# -eq 0 ]; then
+if ((! $#)); then
     echo "cheap bash knockoff of urlwatch"
     echo "urlwatch.sh url-list.txt"
     exit
 fi
 
-urlfile="$1"
-diffargs=""
-cache="$HOME/.urlwatch/cache"
+urlfile=$1
+diffargs=
+cache=$HOME/.urlwatch/cache
 mkdir -p "$cache"
 
 COLOR1='\e[1;32m'
@@ -26,26 +26,27 @@ nap()  # none : none
 
 urlcheck()  # url : pretty print
 {
-    urlhash=$(sha1sum <<< "$1" | cut -d ' ' -f 1)
+    urlhash=$(sha1sum <<< "$1")
+    urlhash=${urlhash%% *}
     temp=$(mktemp)
     curl -s --connect-timeout 10 "$1" > "$temp"
-    if [ ! -s "$temp" ]; then
+    if [[ ! -s $temp ]]; then
         # failure, don't bother anyone with the diff
         rm -f "$temp"
         return
     fi
     html2text -o "$temp.txt" "$temp"
-    if [ ! -e "$cache/$urlhash" ]; then
+    if [[ ! -e $cache/$urlhash ]]; then
         cp "$temp.txt" "$cache/$urlhash"
     fi
-    diff -q "$temp.txt" "$cache/$urlhash" &> /dev/null
-    if [[ "$?" != "0" ]]; then
+    if ! diff -q "$temp.txt" "$cache/$urlhash" &> /dev/null; then
         diff $diffargs "$temp.txt" "$cache/$urlhash" > "$temp.diff"
         cp "$temp.txt" "$cache/$urlhash"
         # echo is atomic, fine for multithreaded stuff
-        echo -e "${COLOR1}${1}${ENDC}\n$(cat $temp.diff)\n\n"
+	# printf because nyeh *raspberry* $ help printf
+        printf '%b\n%s\n\n' "${COLOR1}${1}${ENDC}" "$(< $temp.diff)"
     fi
-    rm -f $temp{,.txt,.diff}
+    rm -f "$temp"{,.txt,.diff}
 }
 
 while read line; do
